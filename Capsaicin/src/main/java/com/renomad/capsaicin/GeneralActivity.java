3f3423e00f7cb3751ca9ff792ed25e8bfb1957e5
Byron Katz <byronka@msn.com>
2013-12-04 08:06:46 -0500

    Starting to assemble the navigation elements

--- Capsaicin/src/main/java/com/renomad/capsaicin/GeneralActivity.java
+++ Capsaicin/src/main/java/com/renomad/capsaicin/GeneralActivity.java
@@ -1,16 +1,41 @@
 package com.renomad.capsaicin;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.VideoView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 
 
-public class GeneralActivity extends Activity {
+public class GeneralActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general);
+        ActionBar actionBar = getSupportActionBar();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        LoaderManager loaderManager = getSupportLoaderManager();
+        ViewPager viewPager = (ViewPager) findViewById(R.id.general_activity_frame);
+
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i2) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
     }
 }
