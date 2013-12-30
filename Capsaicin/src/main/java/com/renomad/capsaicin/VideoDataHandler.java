625f114b056143ba0d502afe4aae7181f55d2c87
Byron Katz <byronka@msn.com>
2013-12-29 23:36:24 -0500

    Starting to add the code necessary to get videos from the server.  Maybe it will also serve to send them to the server.

---
+++ Capsaicin/src/main/java/com/renomad/capsaicin/VideoDataHandler.java
@@ -0,0 +1,13 @@
+package com.renomad.capsaicin;
+
+import android.provider.MediaStore;
+
+/**
+ * Created by Byron on 12/29/13.
+ */
+public class VideoDataHandler {
+
+    public MediaStore.Video GetVideo(int vid) {
+        return new MediaStore.Video();
+    }
+}
