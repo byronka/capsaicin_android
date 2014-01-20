5bc3aa05d14152e1ef049b073565a83bbf15eb9f
Byron Katz <byronka@msn.com>
2014-01-19 20:12:43 -0500

    WIP - not currently compiling.  Enhancing the CtlMsg so that it holds all the information necessary for a stateless request or push.

--- capsaicin/src/com/renomad/capsaicin/CtlMsg.java
+++ capsaicin/src/com/renomad/capsaicin/CtlMsg.java
@@ -6,26 +6,51 @@ import com.renomad.capsaicin.Logger;
 	  *  Control message for data flow to server. Needs to
 	  * be encrypted and secure.  We want our communication
 	  * to be opaque, but fast.
-	  *
+		* see comments in code for capsaicin_server.c to better
+		* understand the protocol. -BK 1/19/2014
+		* @author Byron Katz
 	  */
 	public final class CtlMsg {
 
-		private final byte action;
-		private final byte id;
+		private final ByteBuffer buf;
 
 		//TODO - BK - make sure that the following adheres to best
 		//practices for thread safety
 		//TODO - BK - set up a way to send encoded messages back and forth.
-		//openssl?
+		//openssl?                         
+
+		private CtlMsg(ByteBuffer[] buf) {
+			this.buf = buf.clone();
+		}
+
 		/**
-		  * Creates a thread-safe object for control messages to server.
+		  * Factory method to creat a thread-safe object 
+			* for control messages to server.
 		  * actions are set by the CtlAct static object.
-		  * @param action CtlAct.send or CtlAct.receive
-		  * @param id the video we want
+		  * @param action CtlAct.send or CtlAct.receive, etc.
+		  * @param id the video (or whatever) we want, as a 16 byte UUID.
+		  * @param uid the user we are, as a 16 byte UUID.
+		  * @param origin location within the file to start the file pull.
+			* @param sbytes an integer to say how much to
+			* pull down. (probably make this a constant - should be 
+			* less than 1500 bytes, from the best reading I've 
+			* done on the subject. - BK 1/19/2014
+			* TODO - BK 1/19/2014 - make sure this is totally thread safe.
+			* for example, does it need to be synchronized?
+			* ////TODO - BK - WIP - FINISH THIS PART!
 		  */
-		public CtlMsg(CtlAct action, byte id) {
-			this.action = action.getValue();
-			this.id = id;
+		public static CtlMsg createCtlMsg(CtlAct action, UUID id, UUID uid
+				long origin, short sbytes) {
+			final int current_size_of_msg_packet = 43; //see capsaicin_server.c
+			ByteBuffer mbuf = ByteBuffer.allocate(current_size_of_msg_packet);
+			byte m_action = mbuf.put(0, action.getValue();
+			mbuf.putLong(1,id.getMostSignificantBits());
+			mbuf.putLong(9, id.getLeastSignificantBits();
+			mbuf.putLong(17, uid.getMostSignificantBits();
+			mbuf.putLong(25, uid.getLeastSignificantBits();
+			mbuf.putLong(33, origin.getValue();
+			mbuf.putLong(41, action.getValue();
+			return new CtlMsg(mbuf);
 		}
 
 		/**
@@ -33,7 +58,8 @@ import com.renomad.capsaicin.Logger;
 		  * @return a throw-away byte array
 		  */
 		public byte[] getMessage() {
-			Logger.log("returning a control message of action "+action+" and id "+id+".");
+			Logger.log("returning a control message of action "+
+					action+" and id "+id+".");
 			byte[] msg = {action, id};
 			return msg;
 		}
