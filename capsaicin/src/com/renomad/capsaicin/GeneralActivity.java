9ab31ea01fa8f92ed2c2ff7e336abeed23a3d5a4
Byron Katz <byronka@msn.com>
2014-03-06 17:47:02 -0500

    fixing whitespace.  No other changes intended.

--- capsaicin/src/com/renomad/capsaicin/GeneralActivity.java
+++ capsaicin/src/com/renomad/capsaicin/GeneralActivity.java
@@ -24,17 +24,16 @@ public class GeneralActivity extends ActionBarActivity {
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
         mAppSectionsPagerAdapter = 
-			new AppSectionsPagerAdapter(getSupportFragmentManager());
+	    new AppSectionsPagerAdapter(getSupportFragmentManager());
         viewPager = 
-					(ViewPager) findViewById(R.id.activity_general_viewpager);
-        viewPager.setOnPageChangeListener(
-				new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-							actionBar.setSelectedNavigationItem(position);
+	    (ViewPager) findViewById(R.id.activity_general_viewpager);
+        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+		@Override
+		public void onPageSelected(int position) {
+		    actionBar.setSelectedNavigationItem(position);
 
-            }
-        });
+		}
+	    });
         viewPager.setAdapter(mAppSectionsPagerAdapter);
 
         for (int i = 0; i < mAppSectionsPagerAdapter.getCount(); i++) {
@@ -46,7 +45,7 @@ public class GeneralActivity extends ActionBarActivity {
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment 
-		 * corresponding to one of the primary
+     * corresponding to one of the primary
      * sections of the app.
      */
     public class AppSectionsPagerAdapter extends FragmentPagerAdapter {
@@ -58,16 +57,16 @@ public class GeneralActivity extends ActionBarActivity {
         @Override
         public Fragment getItem(int i) {
             switch (i) {
-                case 0:
-                    return new VideoFragment();
-                case 1:
-                    return new VideoFragment();
-                case 2:
-                    return new VideoFragment();
-                case 3:
-                    return new ProfileFragment();
-                default:
-                    return null;
+	    case 0:
+		return new VideoFragment();
+	    case 1:
+		return new VideoFragment();
+	    case 2:
+		return new VideoFragment();
+	    case 3:
+		return new ProfileFragment();
+	    default:
+		return null;
             }
         }
 
@@ -79,14 +78,14 @@ public class GeneralActivity extends ActionBarActivity {
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
-                case 0:
-                    return "Most Recent";
-                case 1:
-                    return "Most Popular";
-                case 2:
-                    return "Video of the day";
-                case 3:
-                    return "Profile";
+	    case 0:
+		return "Most Recent";
+	    case 1:
+		return "Most Popular";
+	    case 2:
+		return "Video of the day";
+	    case 3:
+		return "Profile";
 
             }
             return "Empty - shouldn't get here";
