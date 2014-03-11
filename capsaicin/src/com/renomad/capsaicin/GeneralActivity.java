0500287d24862fb9389f4327eec4a03f7c9ebf9b
Byron Katz <byronka@msn.com>
2014-03-11 18:31:07 -0400

    Cleaning up and simplifying the code for sending information back to the server.  Nicer now, but there are many todos for the faked information.  Need to make this real and get it talking to the server next.

--- capsaicin/src/com/renomad/capsaicin/GeneralActivity.java
+++ capsaicin/src/com/renomad/capsaicin/GeneralActivity.java
@@ -17,6 +17,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.net.Uri;
 import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.InputStreamReader;
 import java.io.DataOutputStream;
 import java.io.DataInputStream;
@@ -82,14 +86,39 @@ public class GeneralActivity extends ActionBarActivity {
 	}
     }
 
+    private class VideoResult {
+	private final String url;
+	private final int videoSize;
+
+	public String getUrl() {
+	    return url;
+	}
+
+	public int getVideoSize() {
+	    return videoSize;
+	}
+
+	public VideoResult(String url, int videoSize) {
+	    this.url = url;
+	    this.videoSize = videoSize;
+	}
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 	if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
 	    Uri videoUri = data.getData();
 	    Log.i("onActivityResult", "got video back from camera at uri: " + videoUri);
 	    String videoPath = getRealPathFromURI(videoUri);
-	    new UploadFilesTask().execute(videoPath);
+	    int videoSize = getSizeOfVideo(videoUri);
+	    VideoResult vResult = new VideoResult(videoPath, videoSize);
+	    new UploadFilesTask().execute(vResult);
 	}
+    }
+
+    private int getSizeOfVideo(Uri videoUri) {
+	//TODO THIS IS FALSE!! FIX ME WHEn YOU CAN
+	return 1000;
     }
 
     public String getRealPathFromURI(Uri contentUri) {
@@ -103,12 +132,12 @@ public class GeneralActivity extends ActionBarActivity {
     /**
      *   TODO - BK - 3/10/2014 - need to add a progress bar dialog for uploading?
      */
-    private class UploadFilesTask extends AsyncTask<String, Integer, Long> {
-	protected Long doInBackground(String... urls) {
-	    int count = urls.length;
+    private class UploadFilesTask extends AsyncTask<VideoResult, Integer, Long> {
+	protected Long doInBackground(VideoResult... results) {
+	    int count = results.length;
 	    long totalSize = 0;
 	    for (int i = 0; i < count; i++) {
-		totalSize += upLoad2Server(urls[i]);
+		totalSize += uploadToServer(results[i]);
 		publishProgress((int) ((i / (float) count) * 100));
 		// Escape early if cancel() is called
 		//		if (isCancelled()) break;
@@ -125,97 +154,41 @@ public class GeneralActivity extends ActionBarActivity {
 	}
     }
 
-    public static int upLoad2Server(String sourceFileUri) {
-	// String [] string = sourceFileUri;
-	String fileName = sourceFileUri;
-	HttpURLConnection conn = null;
-	DataOutputStream dos = null;
-	DataInputStream inStream = null;
-	String lineEnd = "\r\n";
-	String twoHyphens = "--";
-	String boundary = "*****";
-	int bytesRead, bytesAvailable, bufferSize;
-	int serverResponseCode = 0;
-	byte[] buffer;
-	int maxBufferSize = 1 * 1024 * 1024;
-	String responseFromServer = "";
+    private static int writeStream(OutputStream out) {
+	//TODO send out the stream!
+	return 20; // TODO totally fake!
+    }
 
-	File sourceFile = new File(sourceFileUri);
-	if (!sourceFile.isFile()) {
-	    Log.e("GeneralActivity", "URI does not point at a valid file");
+    private static int readStream(InputStream in) {
+	//TODO read in the stream!
+	return 20; //TODO totally fake!
+    }
+
+    private static int uploadToServer(VideoResult result) {
+	HttpURLConnection urlConnection = null;
+	try {
+	    URL url = new URL(result.getUrl());
+	    urlConnection = (HttpURLConnection) url.openConnection();
+	    urlConnection.setDoOutput(true);
+	    urlConnection.setFixedLengthStreamingMode(result.getVideoSize());
+
+	    OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+	    int bytesWritten = 0;
+	    bytesWritten = writeStream(out);
+
+	    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+	    readStream(in);
+	    return bytesWritten;
+	}
+	catch (Exception e) {
 	    return 0;
 	}
-	try { // open a URL connection to the server
-	    Log.i("upLoad2Server", "about to upload to server");
-	    FileInputStream fileInputStream = new FileInputStream(sourceFile);
-	    Log.i("upLoad2Server", "SERVER_URL is " + SERVER_URL);
-	    URL url = new URL(SERVER_URL);
-	    conn = (HttpURLConnection) url.openConnection(); // Open a HTTP  connection to  the URL
-	    conn.setDoInput(true); // Allow Inputs
-	    conn.setDoOutput(true); // Allow Outputs
-	    conn.setUseCaches(false); // Don't use a Cached Copy
-	    conn.setRequestMethod("POST");
-	    conn.setRequestProperty("Connection", "Keep-Alive");
-	    conn.setRequestProperty("ENCTYPE", "multipart/form-data");
-	    conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-	    conn.setRequestProperty("uploaded_file", fileName);
-	    dos = new DataOutputStream(conn.getOutputStream());
-
-	    dos.writeBytes(twoHyphens + boundary + lineEnd);
-	    dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""+ fileName + "\"" + lineEnd);
-	    dos.writeBytes(lineEnd);
-
-	    bytesAvailable = fileInputStream.available(); // create a buffer of  maximum size
-	    Log.i("GeneralActivity", "Initial .available : " + bytesAvailable);
-
-	    bufferSize = Math.min(bytesAvailable, maxBufferSize);
-	    buffer = new byte[bufferSize];
-
-	    // read file and write it into form...
-	    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-	    while (bytesRead > 0) {
-		dos.write(buffer, 0, bufferSize);
-		bytesAvailable = fileInputStream.available();
-		bufferSize = Math.min(bytesAvailable, maxBufferSize);
-		bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+	finally {
+	    if (urlConnection != null) {
+		urlConnection.disconnect();
 	    }
-
-	    // send multipart form data necesssary after file data...
-	    dos.writeBytes(lineEnd);
-	    dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-
-	    // Responses from the server (code and message)
-	    serverResponseCode = conn.getResponseCode();
-	    String serverResponseMessage = conn.getResponseMessage();
-
-	    Log.i("Upload file to server", "HTTP Response is : " + serverResponseMessage + ": " + serverResponseCode);
-	    // close streams
-	    Log.i("Upload file to server", fileName + " File is written");
-	    fileInputStream.close();
-	    dos.flush();
-	    dos.close();
-	} catch (MalformedURLException ex) {
-	    ex.printStackTrace();
-	    Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
-	} catch (Exception e) {
-	    e.printStackTrace();
 	}
-	//this block will give the response of upload link
-	try {
-	    BufferedReader rd = new BufferedReader(new InputStreamReader(conn
-									 .getInputStream()));
-	    String line;
-	    while ((line = rd.readLine()) != null) {
-		Log.i("GeneralActivity", "RES Message: " + line);
-	    }
-	    rd.close();
-	} catch (IOException ioex) {
-	    Log.e("GeneralActivity", "error: " + ioex.getMessage(), ioex);
-	}
-	return serverResponseCode;  // like 200 (Ok)
-
-    } // end upLoad2Server
+    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment 
@@ -223,6 +196,11 @@ public class GeneralActivity extends ActionBarActivity {
      * sections of the app.
      */
     public class AppSectionsPagerAdapter extends FragmentPagerAdapter {
+
+	VideoFragment vFrag1 = new VideoFragment();
+	VideoFragment vFrag2 = new VideoFragment();
+	VideoFragment vFrag3 = new VideoFragment();
+	ProfileFragment pFrag = new ProfileFragment();
 
         public AppSectionsPagerAdapter(FragmentManager fm) {
             super(fm);
@@ -232,13 +210,13 @@ public class GeneralActivity extends ActionBarActivity {
         public Fragment getItem(int i) {
             switch (i) {
 	    case 0:
-		return new VideoFragment();
+		return vFrag1;
 	    case 1:
-		return new VideoFragment();
+		return vFrag2;
 	    case 2:
-		return new VideoFragment();
+		return vFrag3;
 	    case 3:
-		return new ProfileFragment();
+		return pFrag;
 	    default:
 		return null;
             }
