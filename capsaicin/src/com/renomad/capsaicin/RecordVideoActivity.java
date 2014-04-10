8a31bdefe6b828882b331941a634597b193511fc
Byron Katz <byronka@msn.com>
2014-04-10 13:46:42 -0400

    BK - Adding in some items necessary for the new record video activity.  Why?  Because the standard video recording activity does not pop up in all cases, so I have to take more control.

---
+++ capsaicin/src/com/renomad/capsaicin/RecordVideoActivity.java
@@ -0,0 +1,12 @@
+package com.renomad.capsaicin;
+
+import android.os.Bundle;
+import android.support.v7.app.Activity;
+
+public class RecordVideoActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
