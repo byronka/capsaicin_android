95dacb1abbb6b5943e485379dfd90139ad700f4a
Byron Katz <byronka@msn.com>
2014-04-26 09:51:15 -0400

    Revert "BK - rearranging a couple of permissions, no logic change intended."
    This reverts commit a4eb164adffd1de4d9a458e7f6da4b4045ade3c5.

--- capsaicin/src/com/renomad/capsaicin/Constants.java
+++ capsaicin/src/com/renomad/capsaicin/Constants.java
@@ -1,5 +1,5 @@
 package com.renomad.capsaicin;
 
 public class Constants {
-    public static final String VIDEO_SERVER_URL = "http://192.168.56.2/";
+    public static final String VIDEO_SERVER_URL = "http://capsaicintesting.net/";
 }
